8584509455296ee912ceedb6a29248858939bb2c
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-01 07:50:05 +0100

    routes user

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import Express  from "express";
 const app = Express();
+import userRoutes from "./routes/users.js";
+
+
+
+
+app.use("/api/users", userRoutes)
+
+
+
+
 
 app.listen(8800, ()=>{
   console.log ("L'API TRAVAILLE")
---
+++ api/routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getUser } from "../controllers/user.js";
+
+const router = express.Router()
+
+router.get("/test", getUser)
+
+
+export default router
