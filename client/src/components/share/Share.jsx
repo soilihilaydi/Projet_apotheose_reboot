a3803bf712cd9867e1b330a7a4439780da14bb58
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-01 18:18:21 +0100

    Add posts probleme whit insomnia 28 controller posts

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,31 +1,65 @@
+import React, { useState } from "react"; // Importez React
 import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from "react-query";
+import { makeRequest } from "../../axios";
 
 const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
 
-  const {currentUser} = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+  const queryClient = useQueryClient();
+
+  // Utilisez useMutation pour gérer la mutation d'ajout de post
+  const mutation = useMutation((newPost) => {
+    // Utilisez makeRequest.post ou l'appel à l'API approprié pour ajouter un nouveau post
+    return makeRequest.post("/posts", newPost);
+  },
+  {
+    onSuccess: () => {
+      // Invalider et récupérer
+      queryClient.invalidateQueries("posts");
+    },
+  }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc});
+
+    
+  };
+
   return (
     <div className="share">
       <div className="container">
         <div className="top">
-          <img
-            src={currentUser.profilePic}
-            alt=""
+          <img src={currentUser.profilePic} alt="" />
+          <input
+            type="text"
+            placeholder={`Qu'as-tu en tête ${currentUser.name} ?`}
+            onChange={(e) => setDesc(e.target.value)}
+            value={desc}
           />
-          <input type="text" placeholder={`Qu'as-tu en tête  ${currentUser.name} ?`} />
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
-                <span>Ajouter un image</span>
+                <span>Ajouter une image</span>
               </div>
             </label>
             <div className="item">
@@ -38,7 +72,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Partager</button>
+            <button onClick={handleClick}>Partager</button>
           </div>
         </div>
       </div>
