d95762d6369c67a81cd0cbbf4688e03966ed4aba
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-01 08:12:34 +0100

    auth

---
+++ api/controllers/auth.js
@@ -0,0 +1,20 @@
+export const register = (req,res)=>{
+
+
+  // CHECK l'utilisateur si il existe
+
+
+  // crée un nouvelle utilisateur
+  // hash le mot de passe
+  
+}
+
+
+export const login = (req,res)=>{
+
+}
+
+
+export const logout = (req,res)=>{
+  
+}
