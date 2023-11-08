a5472b17eab3157e01930c83948eefdb0defd999
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-08 18:45:35 +0100

    relationship

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
@@ -48,6 +49,7 @@ app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
+app.use("/api/relationships", relationshipRoutes);
 
 
 app.listen(8800, ()=>{
