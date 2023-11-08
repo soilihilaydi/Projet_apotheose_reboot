93b5ef071824ea8a8faf3cf1c3d39ac21241dd0c
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-08 22:46:31 +0100

    update profile

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,12 +7,36 @@ export const getUser = (req, res) => {
 
   db.query(q, [userId], (err, data) => {
     if (err) return res.status(500).json(err);
-    
-    if (data[0]) { // Vérifiez si data[0] existe
-      const { password, ...info } = data[0];
-      return res.json(info);
-    } else {
-      return res.status(404).json({ message: "Utilisateur non trouvé" });
-    }
+    const { password, ...info } = data[0];
+    return res.json(info);
+  });
+};
+
+export const updateUser = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Non authentifié!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Le jeton n'est pas valide!");
+
+    const q =
+      "UPDATE users SET `name`=?,`city`=?,`website`=?,`profilePic`=?,`coverPic`=? WHERE id=? ";
+
+    db.query(
+      q,
+      [
+        req.body.name,
+        req.body.city,
+        req.body.website,
+        req.body.coverPic,
+        req.body.profilePic,
+        userInfo.id,
+      ],
+      (err, data) => {
+        if (err) res.status(500).json(err);
+        if (data.affectedRows > 0) return res.json("Mis à jour!");
+        return res.status(403).json("Vous pouvez mettre à jour uniquement votre message!");
+      }
+    );
   });
 };
