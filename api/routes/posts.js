a6f375324565a25970386653a2eea156af253742
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-09 10:25:25 +0100

    Revert "button delete"
    This reverts commit c1f518d71dfeead71a4f87d5de4ece0489bdf83a.

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { getPosts, addPost, deletePost } from "../controllers/post.js";
+import { getPosts, addPost } from "../controllers/post.js";
 
 const router = express.Router()
 
 router.get("/", getPosts);
 router.post("/", addPost);
-router.delete("/:id", deletePost);
 
 
 export default router
