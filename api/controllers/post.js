c1f518d71dfeead71a4f87d5de4ece0489bdf83a
soilihi laydi <soilihi.laydi@gmail.com>
2023-11-09 07:26:00 +0100

    button delete

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -50,3 +50,21 @@ export const addPost = (req, res) => {
     });
   });
 };
+
+export const deletePost = (req, res) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json("Non connecté!");
+
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Le jeton n'est pas valide!");
+
+    const q =
+      "DELETE FROM posts WHERE `id`=? AND `userId` = ?";
+
+    db.query(q, [req.params.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if(data.affectedRows>0) return res.status(200).json("Le message a été supprimé.");
+      return res.status(403).json("Vous pouvez supprimer uniquement votre message")
+    });
+  });
+};
